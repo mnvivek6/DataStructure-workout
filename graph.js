980683169a0d0c66a18d06fd690969c971c9a52b
mnvivek6 <vivekmn04@gmail.com>
2024-01-14 17:59:00 +0530

    daily workouts

--- graph.js
+++ graph.js
@@ -348,13 +348,52 @@
 // }
 
 
+// class Graph{
+//     constructor(){
+//         this.adjecencyList={}
+//     }
+//     addVertex(vertex){
+//         if (!this.adjecencyList[vertex]) {
+//             this.adjecencyList[vertex]= new Set
+//         }
+//     }
+//     addEdge(vertex1,vertex2){
+//         if (!this.adjecencyList[vertex1]) {
+//             this.addVertex(vertex1)
+//         }
+//         if (!this.adjecencyList[vertex2]) {
+//             this.adjecencyList(vertex2)
+//         }
+//         this.adjecencyList[vertex1].add(vertex2)
+//         this.adjecencyList[vertex2].add(vertex1)
+//     }
+//     display(){
+//         for (let vertex in this.adjecencyList) {
+//              console.log(vertex+'->'+[...this.adjecencyList[vertex]]);
+            
+//         }
+//     }
+
+
+// }
+
+// const graph = new Graph()
+// graph.addVertex('a')
+// graph.addVertex('b')
+// graph.addVertex('c')
+// graph.addEdge('a','b')
+// graph.display()
+
+
+
+
 class Graph{
     constructor(){
         this.adjecencyList={}
     }
     addVertex(vertex){
         if (!this.adjecencyList[vertex]) {
-            this.adjecencyList[vertex]= new Set
+            this.adjecencyList[vertex] = new Set()
         }
     }
     addEdge(vertex1,vertex2){
@@ -362,24 +401,21 @@ class Graph{
             this.addVertex(vertex1)
         }
         if (!this.adjecencyList[vertex2]) {
-            this.adjecencyList(vertex2)
+            this.addVertex(vertex2)
         }
         this.adjecencyList[vertex1].add(vertex2)
         this.adjecencyList[vertex2].add(vertex1)
     }
     display(){
-        for (let vertex in this.adjecencyList) {
-             console.log(vertex+'->'+[...this.adjecencyList[vertex]]);
-            
+        for (let vertex of this.adjecencyList) {
+            console.log(vertex+'->'+[...this.adjecencyList[vertex]]);
         }
-    }
-
-
+    }   
 }
 
 const graph = new Graph()
 graph.addVertex('a')
 graph.addVertex('b')
 graph.addVertex('c')
-graph.addEdge('a','b')
+graph.addVertex('d')
 graph.display()
