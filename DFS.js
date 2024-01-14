618902e66293bb7ab1366a6e12aca8cd3c284547
mnvivek6 <vivekmn04@gmail.com>
2024-01-14 23:06:01 +0530

    daily workout

--- DFS.js
+++ DFS.js
@@ -1,97 +1,97 @@
-// class Graph{
-//     constructor(){
-//         this.adjlist = new Map()
-//     }
-//     addVertex(vertex){
-//         this.adjlist.set(vertex,new Set())
-//     }
-//     addEdge(vertex1,vertex2){
-//         this.adjlist.get(vertex1).add(vertex2)
-//         this.adjlist.get(vertex2).add(vertex1)
-//     }
-//     getvertexNeighbors(vertex){
-//         return this.adjlist.get(vertex)
-//     }
-//     depthfirstsearchRecursice(vertex,visited = []){
-//         visited.push(vertex)
+class Graph{
+    constructor(){
+        this.adjlist = new Map()
+    }
+    addVertex(vertex){
+        this.adjlist.set(vertex,new Set())
+    }
+    addEdge(vertex1,vertex2){
+        this.adjlist.get(vertex1).add(vertex2)
+        this.adjlist.get(vertex2).add(vertex1)
+    }
+    getvertexNeighbors(vertex){
+        return this.adjlist.get(vertex)
+    }
+    depthfirstsearchRecursice(vertex,visited = []){
+        visited.push(vertex)
     
-//         this.getvertexNeighbors(vertex).forEach(neighbor =>{
-//             if(!visited.includes(neighbor)){
+        this.getvertexNeighbors(vertex).forEach(neighbor =>{
+            if(!visited.includes(neighbor)){
 
-//                 this.depthfirstsearchRecursice(neighbor,visited)
-//             }
+                this.depthfirstsearchRecursice(neighbor,visited)
+            }
 
-//           })
-//         return visited
-//     }
-//    display(){
-//         for(let [vertex,neighbors] of graph.adjlist ){
-//             for(let neighbor of neighbors){
-//                 console.log(`${vertex} --> ${neighbor}`);
-//             }
-//         }
-//    }
-// }
+          })
+        return visited
+    }
+   display(){
+        for(let [vertex,neighbors] of graph.adjlist ){
+            for(let neighbor of neighbors){
+                console.log(`${vertex} --> ${neighbor}`);
+            }
+        }
+   }
+}
 
-// const graph = new Graph()
+const graph = new Graph()
 
-// graph.addVertex("a")
-// graph.addVertex("b")
-// graph.addVertex("c")
-// graph.addVertex("d")
-// graph.addVertex("e")
-// graph.addVertex("f")
-// graph.addVertex("g")
-// graph.addVertex("h")
-// graph.addVertex("i")
-// graph.addVertex("j")
-// graph.addVertex("k")
+graph.addVertex("a")
+graph.addVertex("b")
+graph.addVertex("c")
+graph.addVertex("d")
+graph.addVertex("e")
+graph.addVertex("f")
+graph.addVertex("g")
+graph.addVertex("h")
+graph.addVertex("i")
+graph.addVertex("j")
+graph.addVertex("k")
 
-// graph.addEdge("a","k")
-// graph.addEdge("a","h")
-// graph.addEdge("a","e")
-// graph.addEdge("a","c")
-// graph.addEdge("b","k")
-// graph.addEdge("b","i")
-// graph.addEdge("b","g")
-// graph.addEdge("b","d")
+graph.addEdge("a","k")
+graph.addEdge("a","h")
+graph.addEdge("a","e")
+graph.addEdge("a","c")
+graph.addEdge("b","k")
+graph.addEdge("b","i")
+graph.addEdge("b","g")
+graph.addEdge("b","d")
 
-// graph.display()
+graph.display()
 
-// console.log(graph.getvertexNeighbors("c"));
+console.log(graph.getvertexNeighbors("c"));
 
-// console.log(graph.depthfirstsearchRecursice("a"));
+console.log(graph.depthfirstsearchRecursice("a"));
 // console.log(graph.depthfirstsearchRecursice("d"));
 
 
-class Graph{
-    constructor(){
-        this.adjList = new Map()
-    }
-    addVertex(vertex){
-        this.adjList.set(vertex, new Set())
-    }
-    addEdge(vertex1,vertex2){
-        this.adjList.get(vertex1).set(vertex2)
-        this.adjList.get(vertex2).set(vertex1)
-    }
-    getvtxneighbors(vertex){
-        return this.adjList.get(vertex)
-    }
-    DFS(vertex,visited= []){
-        visited.push(vertex)
-        this.getvtxneighbors(vertex).forEach(neighbor => {
-            if (!visited.includes(neighbor)) {
-                this.DFS(neighbor,visited)
-            }
-        });
-        return visited
-    }
-    display(){
-        for (const [vertex,neighbors] of graph.adjList) {
-            for (const neighbor of neighbors) {
-                console.log(`${vertex}-->${neighbor}`);
-            }
-        }
-    }
-}
+// class Graph{
+//     constructor(){
+//         this.adjList = new Map()
+//     }
+//     addVertex(vertex){
+//         this.adjList.set(vertex, new Set())
+//     }
+//     addEdge(vertex1,vertex2){
+//         this.adjList.get(vertex1).set(vertex2)
+//         this.adjList.get(vertex2).set(vertex1)
+//     }
+//     getvtxneighbors(vertex){
+//         return this.adjList.get(vertex)
+//     }
+//     DFS(vertex,visited= []){
+//         visited.push(vertex)
+//         this.getvtxneighbors(vertex).forEach(neighbor => {
+//             if (!visited.includes(neighbor)) {
+//                 this.DFS(neighbor,visited)
+//             }
+//         });
+//         return visited
+//     }
+//     display(){
+//         for (const [vertex,neighbors] of graph.adjList) {
+//             for (const neighbor of neighbors) {
+//                 console.log(`${vertex}-->${neighbor}`);
+//             }
+//         }
+//     }
+// }
--- graph.js
+++ graph.js
@@ -202,67 +202,68 @@
 // graph.addEdge('A','B')
 // graph.display()
 
-// class Graph{
-//     constructor(){
-//         this.adjecencyList ={}
-//     }
-//     addVertex(vertex) {
-//          if (!this.adjecencyList[vertex]) {
-//             this.adjecencyList[vertex]=new Set()
-//          }
-//     } 
-//     addEdge(vertex1,vertex2){
-//        if (!this.adjecencyList[vertex1]) {
-//         this.addVertex(vertex1)
-//        }
-//        if (!this.adjecencyList[vertex2]) {
-//         this.addVertex(vertex2)
-//        }
-//        this.adjecencyList[vertex1].add(vertex2)
-//        this.adjecencyList[vertex2].add(vertex1)
-//     }
-//     hasEdge(){
-//         return(
-//             this.adjecencyList[vertex1].add(vertex2)&&
-//        this.adjecencyList[vertex2].add(vertex1)
+class Graph{
+    constructor(){
+        this.adjecencyList ={}
+    }
+    addVertex(vertex) {
+         if (!this.adjecencyList[vertex]) {
+            this.adjecencyList[vertex]=new Set()
+         }
+    } 
+    addEdge(vertex1,vertex2){
+       if (!this.adjecencyList[vertex1]) {
+        this.addVertex(vertex1)
+       }
+       if (!this.adjecencyList[vertex2]) {
+        this.addVertex(vertex2)
+       }
+       this.adjecencyList[vertex1].add(vertex2)
+       this.adjecencyList[vertex2].add(vertex1)
+    }
+    hasEdge(vertex1,vertex2){
+        return(
+            this.adjecencyList[vertex1].add(vertex2)&&
+       this.adjecencyList[vertex2].add(vertex1)
 
-//         )
-//     }
-//     removeEdge(vertex1,vertex2){
-//         this.adjecencyList[vertex1].delete(vertex2)
-//         this.adjecencyList[vertex2].delete(vertex1)
-//     }
-//     removeVertex(vertex){
-//         if (!this.adjecencyList[vertex]) {
-//             return
-//         }
-//         for (let adjacentvertex of this.adjecencyList[vertex]) {
-//           this.removeEdge(vertex,adjacentvertex)
-//         }
-//         delete this.adjecencyList[vertex]
-//     }
-//     display(){
-//         for (let vertex in this.adjecencyList) {
-//            console.log(vertex+'->'+[...this.adjecencyList[vertex]]);
+        )
+    }
+    removeEdge(vertex1,vertex2){
+        this.adjecencyList[vertex1].delete(vertex2)
+        this.adjecencyList[vertex2].delete(vertex1)
+    }
+    removeVertex(vertex){
+        if (!this.adjecencyList[vertex]) {
+            return
+        }
+        for (let adjacentvertex of this.adjecencyList[vertex]) {
+          this.removeEdge(vertex,adjacentvertex)
+        }
+        delete this.adjecencyList[vertex]
+    }
+    display(){
+        for (let vertex in this.adjecencyList) {
+           console.log(vertex+'->'+[...this.adjecencyList[vertex]]);
             
-//         }
-//     }
+        }
+    }
    
 
-// }
-// const graph = new Graph()
-// graph.addVertex('A')
-// graph.addVertex('B')
-// graph.addVertex('C')
-// graph.addVertex('D')
-// graph.addEdge('A','D')
-// graph.addEdge('A','B')
-// graph.addEdge('D','C')
-// graph.addEdge('C','F')
+}
+const graph = new Graph()
+graph.addVertex('A')
+graph.addVertex('B')
+graph.addVertex('C')
+graph.addVertex('D')
+graph.addEdge('A','D')
+graph.addEdge('A','B')
+graph.addEdge('D','C')
+graph.addEdge('C','F')
+// console.log(graph.hasEdge('C','A'));
 // graph.removeEdge('A','B')
 // graph.removeEdge('D','C')
 // graph.removeVertex("A")
-// graph.display()
+graph.display()
 
 // class Graph{
 //     constructor(){
@@ -387,35 +388,35 @@
 
 
 
-class Graph{
-    constructor(){
-        this.adjecencyList={}
-    }
-    addVertex(vertex){
-        if (!this.adjecencyList[vertex]) {
-            this.adjecencyList[vertex] = new Set()
-        }
-    }
-    addEdge(vertex1,vertex2){
-        if (!this.adjecencyList[vertex1]) {
-            this.addVertex(vertex1)
-        }
-        if (!this.adjecencyList[vertex2]) {
-            this.addVertex(vertex2)
-        }
-        this.adjecencyList[vertex1].add(vertex2)
-        this.adjecencyList[vertex2].add(vertex1)
-    }
-    display(){
-        for (let vertex of this.adjecencyList) {
-            console.log(vertex+'->'+[...this.adjecencyList[vertex]]);
-        }
-    }   
-}
+// class Graph{
+//     constructor(){
+//         this.adjecencyList={}
+//     }
+//     addVertex(vertex){
+//         if (!this.adjecencyList[vertex]) {
+//             this.adjecencyList[vertex] = new Set()
+//         }
+//     }
+//     addEdge(vertex1,vertex2){
+//         if (!this.adjecencyList[vertex1]) {
+//             this.addVertex(vertex1)
+//         }
+//         if (!this.adjecencyList[vertex2]) {
+//             this.addVertex(vertex2)
+//         }
+//         this.adjecencyList[vertex1].add(vertex2)
+//         this.adjecencyList[vertex2].add(vertex1)
+//     }
+//     display(){
+//         for (let vertex in this.adjecencyList) {
+//             console.log(vertex+'->'+[...this.adjecencyList[vertex]]);
+//         }
+//     }   
+// }
 
-const graph = new Graph()
-graph.addVertex('a')
-graph.addVertex('b')
-graph.addVertex('c')
-graph.addVertex('d')
-graph.display()
+// const graph = new Graph()
+// graph.addVertex('a')
+// graph.addVertex('b')
+// graph.addVertex('c')
+// graph.addVertex('d')
+// graph.display()
